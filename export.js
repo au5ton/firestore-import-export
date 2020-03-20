79108abd59a44f2e64b904c696122e399d227b81
Austin Jackson <themacphage@gmail.com>
2020-03-20 06:15:57 -0500

    tweaked for personal one-time use

--- export.js
+++ export.js
@@ -1,14 +1,20 @@
+
 const admin = require("firebase-admin");
 const fs = require('fs');
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("/home/austin/.config/me/cougargrades-aefb6.firebaseadminsdk.json");
+
+let i = 0;
+let j = 0;
 
 let collectionName = process.argv[2];
 let subCollection = process.argv[3];
+console.log(`collectionName: ${collectionName}`)
+console.log(`subCollection:  ${subCollection}`)
 
 // You should replace databaseURL with your own
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ionic-firestore-dn.firebaseio.com"
+  databaseURL: "https://cougargrades-aefb6.firebaseio.com"
 });
 
 let db = admin.firestore();
@@ -19,17 +25,24 @@ data[collectionName] = {};
 
 let results = db.collection(collectionName)
 .get()
-.then(snapshot => {
-  snapshot.forEach(doc => {
-    data[collectionName][doc.id] = doc.data();
-  })
-  return data;
+.then(async snapshot => {
+  console.log(`Query size: ${snapshot.size}`)
+  for(let doc of snapshot.docs) {
+    fs.mkdirSync(`export/${collectionName}`, { recursive: true })
+    fs.writeFileSync(`export/${collectionName}/${doc.id}.json`, JSON.stringify(doc.data()))
+    console.log(`${++i} : \t ${collectionName} \t ${doc.id}`)
+    if(subCollection) {
+        await addSubCollection(doc.id, {})
+    }
+    //data[collectionName][doc.id] = doc.data();
+  }
+  //return data;
 })
 .catch(error => {
   console.log(error);
 })
 
-results.then(dt => {  
+/*results.then(dt => {  
   getSubCollection(dt).then(() => {    
     // Write collection to JSON file
     fs.writeFile("firestore-export.json", JSON.stringify(data), function(err) {
@@ -39,7 +52,7 @@ results.then(dt => {
         console.log("The file was saved!");
     });
   })  
-})
+})*/
 
 async function getSubCollection(dt){
   for (let [key, value] of Object.entries([dt[collectionName]][0])){
@@ -54,8 +67,11 @@ function addSubCollection(key, subData){
   return new Promise(resolve => {
     db.collection(collectionName).doc(key).collection(subCollection).get()
     .then(snapshot => {      
-      snapshot.forEach(subDoc => {             
-        subData[subDoc.id] =  subDoc.data();
+      snapshot.forEach(subDoc => {
+        fs.mkdirSync(`export/${collectionName}/${key}/${subCollection}`, { recursive: true })
+        fs.writeFileSync(`export/${collectionName}/${key}/${subCollection}/${subDoc.id}.json`, JSON.stringify(subDoc.data()))
+        console.log(`\t${++j} : \t ${subCollection} \t ${subDoc.id}`)
+        //subData[subDoc.id] =  subDoc.data();
         resolve('Added data');                                                                 
       })
     })
